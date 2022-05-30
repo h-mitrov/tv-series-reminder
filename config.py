cc4abe956d09044f6a82421bbc27763de8b1f39c
Hennadii Mitrov <mitrovgn@gmail.com>
2022-05-30 18:35:19 +0300

    improved config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
-API_KEY = ''
-MAIL_SERVER = ''
-MAIL_PORT = '465'
-MAIL_USERNAME = ''
-MAIL_PASSWORD = ''
-MAIL_USE_TLS = False
-MAIL_USE_SSL = True
+import os
+
+API_KEY = os.environ.get('API_KEY')
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+MAIL_PORT = os.environ.get('MAIL_PORT')
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_USE_TLS = False                     # False for 465 port
+MAIL_USE_SSL = True                      # True for 465 port
